8e559a2dc20eaf64f4096f9eedb1b91e641ff1bd
Allan Pichardo <allan.pichardo@gmail.com>
2019-01-31 17:23:49 -0500

    First commit

---
+++ Assets/Scripts/WalkableGrid.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkableGrid : MonoBehaviour
+{
+    public enum BendEdge
+    {
+        North, South, East, West, Top
+    }
+
+    public List<BendEdge> bendEdges;
+    public int width = 9;
+    public int height = 9;
+    public GameObject oddTile;
+    public GameObject evenTile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GenerateGrid()
+    {
+        if (isValid())
+        {
+            GeneratePlane(Vector3.zero, Vector3.zero);
+            for (int i = 0; i < bendEdges.Count; i++)
+            {
+                Vector3 rotation = getRotationForWall(i);
+                Vector3 translation = getTranslationForWall(i);
+                GeneratePlane(translation,rotation);
+            }
+        }
+    }
+
+    private Vector3 getRotationForWall(int i)
+    {
+        Vector3 rotation = Vector3.zero;
+        
+        switch (bendEdges[i])
+        {
+            case BendEdge.North:
+                rotation.x = 90;
+                break;
+            case BendEdge.South:
+                break;
+            case BendEdge.East:
+                break;
+            case BendEdge.West:
+                break;
+            case BendEdge.Top:
+                break;
+        }
+
+        return rotation;
+    }
+
+    private Vector3 getTranslationForWall(int i)
+    {
+        Vector3 vector = new Vector3();
+        
+        switch (bendEdges[i])
+        {
+            case BendEdge.North:
+                vector.z = height;
+                break;
+            case BendEdge.South:
+                vector.z = -height;
+                break;
+            case BendEdge.East:
+                vector.x = width;
+                break;
+            case BendEdge.West:
+                vector.x = -width;
+                break;
+            case BendEdge.Top:
+                vector.y = height;
+                break;
+        }
+
+        return vector;
+    }
+
+    private void GeneratePlane(Vector3 translation, Vector3 rotation)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                GameObject newTile = Instantiate((i + j) % 2 == 0 ? evenTile : oddTile, this.transform);
+                newTile.transform.localPosition = new Vector3(j, 0, i) + translation;
+                newTile.transform.Rotate(rotation);
+            }
+        }
+    }
+
+    private bool isValid()
+    {
+        return width > 0 && height > 0 && oddTile != null && evenTile != null;
+    }
+}
---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    private Vector3 currentDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDirection = transform.TransformDirection(Vector3.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CanMoveForward();
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Changing direction");
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        currentDirection = Vector3.Cross(transform.TransformDirection(Vector3.up), currentDirection);
+    }
+   
+    private bool CanMoveForward()
+    {
+        int layerMask = 1 << 10;
+
+        var position = transform.position;
+        Debug.DrawRay(position, currentDirection * 1000, Color.black, Time.deltaTime);
+        return Physics.Raycast(position, currentDirection, 1.0f, layerMask);
+    }
+}
