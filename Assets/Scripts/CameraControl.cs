2c3bb5f7c6672653d7451e3e18b7a0838b85fd1a
Allan Pichardo <allan.pichardo@gmail.com>
2019-02-06 10:43:44 -0500

    Fixed issues with camera angle

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraControl : MonoBehaviour
+{
+    public enum CameraDirection
+    {
+        North, South, East, West
+    }
+    
+    private Vector3 lastMousePos = Vector3.zero;
+    private bool isMouseHeld;
+
+    public float maxRotation = 30.0f;
+    public float lookSpeed = 0.7f;
+    public CameraDirection direction = CameraDirection.North;
+
+    private void Start()
+    {
+        lastMousePos = Input.mousePosition;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMouseHeld = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseHeld = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RotateCameraDirection(true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RotateCameraDirection(false);
+        }
+        
+        AdjustCameraLook(Input.mousePosition);
+    }
+    
+    public void RotateCameraDirection(bool toRight)
+    {
+        float angle = toRight ? 90.0f : -90.0f;
+        Vector3 parentCenter = transform.parent.TransformPoint(new Vector3(-1,0,1));
+        transform.RotateAround(parentCenter, Vector3.up, angle);
+    }
+
+    private CameraDirection GetNextDirection()
+    {
+        switch (direction)
+        {
+            case CameraDirection.North:
+                return CameraDirection.East;
+            case CameraDirection.East:
+                return CameraDirection.South;
+            case CameraDirection.South:
+                return CameraDirection.West;
+            case CameraDirection.West:
+                return CameraDirection.North;
+            default:
+                return direction;
+        }
+    }
+
+    public void AdjustCameraLook(Vector3 position)
+    {
+        Vector3 mouseDelta = (position - lastMousePos);
+
+        if (isMouseHeld)
+        {
+            transform.Rotate(new Vector3(mouseDelta.y * lookSpeed, 0, 0), Space.Self);
+        }
+
+        lastMousePos = position;
+    }
+    
+}
--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Walker : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float lookDistance = 0.5f;
     private Vector3 currentDirection;
-    private Rigidbody rigidbody;
     private const float SpeedScale = 0.1f;
-    
+    private Animator animator;
+    private Collider collider;
+    private static readonly int Move = Animator.StringToHash("move");
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         currentDirection = transform.TransformDirection(Vector3.forward);
-        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -28,12 +32,17 @@ public class Walker : MonoBehaviour
 
     private void TakeStep()
     {
+        if (animator != null)
+        {
+            animator.SetTrigger(Move);
+        }
         transform.Translate(currentDirection * speed * SpeedScale, Space.World);
     }
 
     private void ChangeDirection()
     {
         currentDirection = Vector3.Cross(transform.TransformDirection(Vector3.up), currentDirection);
+        transform.Rotate(transform.TransformDirection(Vector3.up), 90.0f);
     }
    
     private bool CanMoveForward()
@@ -41,7 +50,18 @@ public class Walker : MonoBehaviour
         int layerMask = 1 << 10;
 
         var position = transform.position;
-        Debug.DrawRay(position, currentDirection * 0.5f, Color.yellow, Time.deltaTime);
-        return !Physics.Raycast(position, currentDirection, 0.5f, layerMask);
+        
+        Ray ray = new Ray(position, currentDirection);
+        bool isDetected = Physics.Raycast(ray, lookDistance, layerMask);
+        if (isDetected)
+        {
+            Debug.DrawRay(position, currentDirection * lookDistance, Color.red, Time.deltaTime);
+        }
+        else
+        {
+            Debug.DrawRay(position, currentDirection * lookDistance, Color.white, Time.deltaTime);
+        }
+
+        return !isDetected;
     }
 }
