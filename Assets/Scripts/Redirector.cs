30c64727e9894fb0ebd212c0f65ba42c0995b25b
Allan Pichardo <allan.pichardo@gmail.com>
2019-02-12 20:32:41 -0500

    Added overlay with instructions

---
+++ Assets/Scripts/Redirector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Redirector : MonoBehaviour
+{
+    public Material UpMaterial;
+    public Material DownMaterial;
+    public Material LeftMaterial;
+    public Material RightMaterial;
+    
+    public enum Direction
+    {
+        Up, Down, Left, Right, None
+    }
+
+    public Direction direction = Direction.None;
+
+    private Material defaultMaterial;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMaterial = meshRenderer.material;
+    }
+
+    void Update()
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                meshRenderer.material = UpMaterial;
+                break;
+            case Direction.Down:
+                meshRenderer.material = DownMaterial;
+                break;
+            case Direction.Left:
+                meshRenderer.material = LeftMaterial;
+                break;
+            case Direction.Right:
+                meshRenderer.material = RightMaterial;
+                break;
+            case Direction.None:
+                meshRenderer.material = defaultMaterial;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+    }
+    
+}
---
+++ Assets/Scripts/TileSelector.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileSelector : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject tilePanel;
+    public List<Redirector.Direction> availableActions;
+    private Stack<Redirector.Direction> directions;
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+
+    private void Start()
+    {
+        directions = new Stack<Redirector.Direction>(availableActions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider != null && hit.collider.gameObject.layer == Layers.Floor)
+            {
+                Redirector redirector = hit.collider.gameObject.GetComponent<Redirector>();
+
+                if (redirector.direction == Redirector.Direction.None)
+                {
+                    redirector.direction = directions.Pop();
+                }
+                else
+                {
+                    directions.Push(redirector.direction);
+                    redirector.direction = Redirector.Direction.None;
+                }
+            }
+        }
+
+        if (tilePanel != null)
+        {
+            foreach (var direction in directions)
+            {
+                
+            }
+        }
+
+    }
+
+    private GameObject GetArrowIcon(Redirector.Direction direction)
+    {
+        GameObject NewObj = new GameObject(); //Create the GameObject
+        Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
+        switch (direction)
+        {
+            case Redirector.Direction.Up:
+                NewImage.sprite = upSprite;
+                break;
+            case Redirector.Direction.Down:
+                NewImage.sprite = downSprite;
+                break;
+            case Redirector.Direction.Left:
+                NewImage.sprite = leftSprite;
+                break;
+            case Redirector.Direction.Right:
+                NewImage.sprite = rightSprite;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return NewObj;
+    }
+}
